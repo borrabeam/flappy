909d7f7cfe2bcded95b8c816983c17dd9408ce80
borrabeam <bannakorn.s@ku.th>
2021-03-18 01:22:14 +0700

    OnKeyPressed

--- flappydot.py
+++ flappydot.py
@@ -8,27 +8,40 @@ CANVAS_HEIGHT = 500
 UPDATE_DELAY = 33
 GRAVITY = 2.5
 STARTING_VELOCITY = -30
+JUMP_VELOCITY = -20
 
 class Dot(Sprite):
     def init_element(self):
         # self.vy = -30
         self.vy = STARTING_VELOCITY
+        self.is_started = False
+
 
     def update(self):
-        self.y += self.vy
-        self.vy += GRAVITY
- 
+        # self.y += self.vy
+        # self.vy += GRAVITY
+        if self.is_started:
+            self.y += self.vy
+            self.vy += GRAVITY
+
+
+    def start(self):
+        self.is_started = True
+    
+
+    #  def jump(self):
+    #     self.vu = JUMP_VELOCITY
 
 
 class FlappyGame(GameApp):
     def create_sprites(self):
         self.dot = Dot(self, 'images/dot.png', CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
-
         self.elements.append(self.dot)
+        
 
     def init_game(self):
         self.create_sprites()
-
+        self.is_started = False
     def pre_update(self):
         pass
 
@@ -36,7 +49,12 @@ class FlappyGame(GameApp):
         pass
 
     def on_key_pressed(self, event):
-        pass
+        if event.char == " ":
+            self.dot.start()
+            self.dot.jump()
+
+            for i in self.elements[1:]:
+                i.start()
 
 
 if __name__ == "__main__":
